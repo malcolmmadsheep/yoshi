6fbb5fac5c0b50fe63d9b0996245424314d5f856
Yaniv Efraim <yaniv.efraim@gmail.com>
2020-03-23 10:55:04 +0200

    tests

--- test/javascript/features/yoshi-server/yoshi-server-testing/src/api/greeting.api.js
+++
@@ -1,8 +0,0 @@
-import { fn } from 'yoshi-server';
-
-export const greet = fn(function(name) {
-  return {
-    greeting: `hello ${name}`,
-    name,
-  };
-});
--- test/javascript/features/yoshi-server/yoshi-server-testing/src/routes/app.js
+++
@@ -1,7 +0,0 @@
-import { route, render } from 'yoshi-server';
-
-export default route(async function() {
-  const html = await render('app');
-
-  return html;
-});
